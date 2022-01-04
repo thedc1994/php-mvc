1b1cb1ef34dc3675d80440ae4f87ce4de8c63e1a
Đinh Công Thế <thedc@omt.vn>
2022-01-04 23:17:50 +0700

    add layouts

--- views/home.php
+++ views/home.php
@@ -1,47 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-  <meta charset="UTF-8">
-  <title>Test MVC</title>
-  <meta name="csrf-token"/>
-  <meta name="viewport" content="width=device-width,initial-scale=1">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+<?php include 'views/layouts/master.php' ?>
 
-  <body>
+<?php startblock('title') ?>
+    Trang chủ
+<?php endblock() ?>
 
-    <div class="row" style="margin-top: 100px;">
-      <div class="col-2">
-      </div>
-      <div class="col-4">
-          <form action="<?php echo Route::name('auth.register');?>" method="POST">
-              <div class="row form-group">
-                  <label>Tên tài khoản</label>
-                  <input type="text" class="form-control" placeholder="Nhập tên tài khoản" name="username">
-              </div>
-              <div class="row form-group">
+
+<?php startblock('content') ?>
+    <div class="col-2">
+    </div>
+    <div class="col-4">
+        <form action="<?php echo Route::name('auth.register');?>" method="POST">
+            <div class="row form-group">
+                <label>Tên tài khoản</label>
+                <input type="text" class="form-control" placeholder="Nhập tên tài khoản" name="username">
+            </div>
+            <div class="row form-group">
                 <button class="btn btn-success" type="submit">
                     ĐĂNG KÝ
                 </button>
-              </div>
-          </form>
-      </div>
+            </div>
+        </form>
     </div>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-            crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.8/js/select2.min.js" defer></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"
-            defer></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-            crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script>
-        $(document).ready(function () {
+<?php endblock() ?>
 
-        });
-    </script>
-  </body>
-</html>
